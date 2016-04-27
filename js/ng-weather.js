65b905af26ef39cf1d479cc9e14532b13af25fdb
Tony Samperi <tonysamperibg89@gmail.com>
2016-04-27 16:38:40 +0200

    V1.3
    Better language handling, added location setting, added "last updated",
    handled city not found (service will not raise error, so I replace the
    city), moved template to external file, added missing weather
    conditions, misc.

--- js/ng-weather.js
+++ js/ng-weather.js
@@ -19,8 +19,11 @@ angular.module("ngWeather", [])
 		scope.isLoading = true;
 		if(!!attrs.city && !!attrs.appId)
 			scope.getTemp(attrs.city, attrs.appId);
-		else
-			console.error("INVALID CITY!");
+		else{
+			$scope.isLoading = false;
+			$scope.isError = true;
+			$scope.errorMessage = data[lang]["configerror"];
+		}
 	};
 
 	return {
@@ -28,12 +31,33 @@ angular.module("ngWeather", [])
 		replace: true,
 		//require: "^city",
 		scope: { city: '@', appId: '@'},
-		controller: ["$http", "$scope", "$locale", function($http, $scope,$locale){
+		controller: ["$http", "$scope", "$element","$locale", '$filter',"$timeout", function($http, $scope,$element,$locale,$filter,$timeout){
+
+			var cmp = function(s1,s2){
+				s1 = s1.toLowerCase();
+				s2 = s2.toLowerCase();
+				return (s1.indexOf(s2) < 0);
+			}
+			var data = {
+				it: {
+					srverror: "Non sono riuscito a recuperare i dati.",
+					configerror: "Ci sono alcuni errori nella configurazione!",
+					updatedAtText: "Ultimo aggiornamento",
+					updatedAtDate: "Oggi alle "
+				},
+				en: {
+					srverror: "Data can't be retrieved.",
+					configerror: "There are some errors in your config!",
+					updatedAtText: "Last update",
+					updatedAtDate: "Today at "
+				}
+			}
 			console.log("%c %s", "color:orange; background:blue; font-size: 16pt", "LOCALE");
 			console.debug($locale);
 			var locale = $locale.id;
-			var units = !!$locale.id && $locale.id.indexOf("en")>-1 ? "imperial" : "metric"
-			$scope.unit = units == "metric" ? "°C" : "°F";
+			var lang = !!$locale.id && $locale.id.indexOf("IT")>-1 ? "it" : "en";
+			var units = lang == "it" ? "metric" : "imperial";
+			$scope.unit = lang == "it" ? "°C" : "°F";
 			var apiUrl = "http://api.openweathermap.org/data/2.5/weather";
 			var params = "mode=json&units="+units+"&cnt=7&callback=JSON_CALLBACK";
 			$scope.getTemp = function(city, appId){
@@ -45,6 +69,10 @@ angular.module("ngWeather", [])
 				};
 				$http(config).then(function(r){
 					var response = r.data;
+					var name = r.data.name;
+					if(cmp($scope.city,name)) $scope.city = name;
+					$scope.updatedAtText = data[lang]["updatedAtText"];
+					$scope.updatedAtDate = data[lang]["updatedAtDate"] + $filter("date")(new Date(),"HH:mm");
 					$scope.weather = response.weather[0].main;
 					$scope.weatherCode = response.weather[0].id;
 					$scope.temp = parseInt(response.main.temp);
@@ -52,12 +80,13 @@ angular.module("ngWeather", [])
 				}, function(){
 					$scope.isLoading = false;
 					$scope.isError = true;
-					$scope.errorMessage = "Data can't be retrieved.";
-					console.error("ERROR RETRIEVING WEATHER");
+					$scope.errorMessage = data[lang]["srverror"];
 				});
 			};
 
-			$scope.reload = function(){
+			$scope.reload = function(city){
+				if(!!city) $scope.city = city;
+				$scope.showSettings = false;
 				$scope.isError = false;
 				$scope.isLoading = true;
 				if(!!$scope.city && $scope.appId)
@@ -65,29 +94,18 @@ angular.module("ngWeather", [])
 				else{
 					$scope.isLoading = false;
 					$scope.isError = true;
-					$scope.errorMessage = "There are some errors in your config!";
+					$scope.errorMessage = data[lang]["configerror"];
 				}
+			};
+
+			$scope.setLocation = function(){
+				$scope.showSettings = true;
+				$timeout(function(){
+					$element.find(".ngw-input").focus();
+				},250);
 			};
 		}],
 		link: linkFn,
-		template: '<div class="ngw-container ngw-row {{weather}}" ngw-spinner="isLoading">\
-		<span class="glyphicon glyphicon-refresh ngw-reload" ng-click="reload()"></span>\
-		<div ng-if="!isError">\
-		<div class="ngw-col">\
-		<div class="ngw-icon"><i class="owf owf-{{weatherCode}}" aria-label="{{weather}}"></i></div>\
-		</div>\
-		<div class="ngw-col">\
-		<div class="ngw-temp"><span ng-bind="temp || \'-\'"></span>&nbsp;<span ng-bind="unit"></span></div>\
-		<div class="ngw-location">\
-		<i class="glyphicon glyphicon-pushpin" aria-label="Location"></i><span ng-bind="city"></span>\
-		</div>\
-		</div>\
-		</div>\
-		<div class="ngw-col" ng-if="isError">\
-		<span ng-bind="errorMessage"></span>\
-		</div>\
-		<div class="ngw-clearfix">\
-		</div>\
-		</div>'
+		templateUrl: "./ngwTpl.html"
 	};
 });
